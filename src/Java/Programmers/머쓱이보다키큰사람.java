36b135e3df6f8d2ad86c0d145ce0dce5dd944493
JungGyu Ahn(안중규) <newending01@gmail.com>
2023-01-17 16:52:19 +0900

    머쓱이보다 키 큰 사람

---
+++ src/Java/Programmers/머쓱이보다키큰사람.java
@@ -0,0 +1,14 @@
+package Java.Programmers;
+
+public class 머쓱이보다키큰사람 {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > height) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
